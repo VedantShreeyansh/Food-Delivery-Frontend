785cbace39dc2a32c5f4d161bc9a3f39b683a391
Vedant <vedant1204jsg@gmail.com>
2025-07-04 17:59:02 +0530

    3rd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
